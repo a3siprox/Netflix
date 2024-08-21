928090717fa2abce25a187829a38e978a4c970f6
a3siprox <abdulmejidsetegn4@gmail.com>
2024-08-21 16:36:29 +0300

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Rows/RowList/RowList";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
